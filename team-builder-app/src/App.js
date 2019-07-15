b34ccfaaa94bdfb090df716686b9b2f42ed9fb5a
Clark Williams <theclarkwilliams@gmail.com>
2019-07-15 15:04:30 -0600

    step 1 complete

--- team-builder-app/src/App.js
+++ team-builder-app/src/App.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
 import Form from "./components/Form";
+import TeamMember from "./components/TeamMember";
 
 function App() {
   
-  const [list, setList] = useState("list")
-  // const [list, setList] = useState({
-  //   name: "",
-  //   email: "",
-  //   role: ""
-  // })
+  const [list, setList] = useState([
+    {
+      name: "Bill",
+      email: "bill@email.com",
+      role: "Front End"
+    },
+    {
+      name: "Terminator",
+      email: "illbebahk@msn.com",
+      role: "Kill stuff"
+    }
+  ])
+
   
   return (
     <div className="App">
-      {/* <div>{list}</div> */}
-      <div>list</div>
+      {list.map(member => {
+        return <TeamMember member={member} key={member.name} />
+      })}
       <Form />
     </div>
   );
